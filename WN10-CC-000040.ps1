9a930ca492631f8636b1c4c61f03f98a97eedaa2
Darya M <31021274+Darya-cybersec@users.noreply.github.com>
2025-12-16 12:57:18 -0500

    Update WN10-CC-000040.ps1

--- WN10-CC-000040.ps1
+++ WN10-CC-000040.ps1
@@ -1,1 +1,27 @@
 
+<#
+.SYNOPSIS
+    This PowerShell script disables insecure guest logons to ensure that access to shared resources requires authentication, preventing unauthenticated access and ensuring compliance with DISA STIG WN10-CC-000040.
+    
+.NOTES
+    Author          : Darya Murha
+    LinkedIn        : www.linkedin.com/in/darya-cybersec
+    GitHub          : https://github.com/Darya-cybersec
+    Date Created    : 2025-16-12
+    Last Modified   : 2025-16-12
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000040
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000040).ps1 
+#>
