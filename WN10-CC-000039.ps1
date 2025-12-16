31f10db409fadd5d6a4d5d2cb49a8b993d7acbf5
Darya M <31021274+Darya-cybersec@users.noreply.github.com>
2025-12-16 12:20:22 -0500

    Update WN10-CC-000039.ps1

--- WN10-CC-000039.ps1
+++ WN10-CC-000039.ps1
@@ -1,1 +1,26 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables WDigest authentication to prevent plaintext credentials from being stored in LSASS, mitigating the risk of credential theft and ensuring compliance with DISA STIG WN10-CC-000038.
 
+.NOTES
+    Author          : Darya Murha
+    LinkedIn        : www.linkedin.com/in/darya-cybersec
+    GitHub          : https://github.com/Darya-cybersec
+    Date Created    : 2025-16-12
+    Last Modified   : 2025-16-12
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000039
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000039).ps1 
+#>
