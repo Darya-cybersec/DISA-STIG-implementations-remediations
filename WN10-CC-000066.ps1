864257d3850679ae3ae54713f6e2399569bb8839
Darya M <31021274+Darya-cybersec@users.noreply.github.com>
2025-12-16 13:22:53 -0500

    Update WN10-CC-000066.ps1

--- WN10-CC-000066.ps1
+++ WN10-CC-000066.ps1
@@ -1,1 +1,27 @@
 
+<#
+.SYNOPSIS
+    
+    
+.NOTES
+    Author          : Darya Murha
+    LinkedIn        : www.linkedin.com/in/darya-cybersec
+    GitHub          : https://github.com/Darya-cybersec
+    Date Created    : 2025-16-12
+    Last Modified   : 2025-16-12
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000066
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000066).ps1 
+
