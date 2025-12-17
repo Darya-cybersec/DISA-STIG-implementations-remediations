bd796bb609e2ae852e3a88f00ce050bba62445ee
Darya M <31021274+Darya-cybersec@users.noreply.github.com>
2025-12-17 10:39:13 -0500

    Update WN10-CC-000197.ps1

--- WN10-CC-000197.ps1
+++ WN10-CC-000197.ps1
@@ -1,1 +1,28 @@
 
+<#
+.SYNOPSIS
+    This PowerShell script disables AutoPlay for non-volume devices to prevent automatic execution of potentially malicious content when removable media is connected, ensuring compliance with DISA STIG WN10-CC-000180.
+    
+.NOTES
+    Author          : Darya Murha
+    LinkedIn        : www.linkedin.com/in/darya-cybersec
+    GitHub          : https://github.com/Darya-cybersec
+    Date Created    : 2025-17-12
+    Last Modified   : 2025-17-12
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000180
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000180).ps1 
+
+    #>
