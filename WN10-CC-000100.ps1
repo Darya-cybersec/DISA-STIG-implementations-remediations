e776341935a78f64bfecfa596a8a06fcecd706e8
Darya M <31021274+Darya-cybersec@users.noreply.github.com>
2025-12-17 13:51:50 -0500

    Update WN10-CC-000100.ps1

--- WN10-CC-000100.ps1
+++ WN10-CC-000100.ps1
@@ -1,1 +1,30 @@
 
+<#
+.SYNOPSIS
+    This PowerShell script disables Microsoft Consumer Experiences to prevent the automatic installation of suggested apps and notifications, helping organizations maintain controlled application execution in compliance with DISA STIG WN10-CC-000197.
+    
+.NOTES
+    Author          : Darya Murha
+    LinkedIn        : www.linkedin.com/in/darya-cybersec
+    GitHub          : https://github.com/Darya-cybersec
+    Date Created    : 2025-17-12
+    Last Modified   : 2025-17-12
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000100
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000100).ps1 
+
+    #>
+
+
