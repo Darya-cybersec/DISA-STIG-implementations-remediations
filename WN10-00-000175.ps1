01e96de176b20a590e6528647eeb2b34313580a8
Darya M <31021274+Darya-cybersec@users.noreply.github.com>
2025-12-17 13:22:05 -0500

    Update and rename WN10-00-000175 to WN10-00-000175.ps1

--- WN10-00-000175.ps1
+++ WN10-00-000175.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables AutoPlay for non-volume devices to prevent automatic execution of potentially malicious content when removable media is connected, ensuring compliance with DISA STIG WN10-CC-000180.
+    
+.NOTES
+    Author          : Darya Murha
+    LinkedIn        : www.linkedin.com/in/darya-cybersec
+    GitHub          : https://github.com/Darya-cybersec
+    Date Created    : 2025-17-12
+    Last Modified   : 2025-17-12
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-00-000175
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-00-000175).ps1 
+
+    #>
+
 # STEP 1: Confirm PowerShell is running as Administrator
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
 ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
